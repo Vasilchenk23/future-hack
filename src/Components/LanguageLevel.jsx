ec5680518feecb3a214322a4e6f0e37865c5ae3c
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-04-26 00:47:56 +0300

    color svg

--- src/Components/LanguageLevel.jsx
+++ src/Components/LanguageLevel.jsx
@@ -37,7 +37,7 @@ const LanguageLevel = () => {
                         </svg>
                         <Link to="/main">
                             <svg  xmlns="http://www.w3.org/2000/svg"  width="70"  height="70"  viewBox="0 0 24 24"  fill="currentColor"  stroke="currentColor"  strokeWidth="2"  
-                                strokeLinecap="round"  strokeLinejoin="round" className="arrow-left">
+                                strokeLinecap="round"  stroke-linejoin="round" className="arrow-left">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M5 12l6 6" /><path d="M5 12l6 -6" />
                             </svg>
                         </Link>
--- src/Components/Speaking.jsx
+++ src/Components/Speaking.jsx
@@ -63,7 +63,7 @@ const Speaking = () => {
             </svg>
             <Link to="/goals" style={{ color: textColor }}>
               <svg  xmlns="http://www.w3.org/2000/svg"  width="70"  height="70"  viewBox="0 0 24 24"  fill="currentColor"  stroke="currentColor"  strokeWidth="2"  
-              strokeLinecap="round"  strokeLinejoin="round" className="arrow-left-speak">
+              strokeLinecap="round"  stroke-linejoin="round" className="arrow-left-speak">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M5 12l6 6" /><path d="M5 12l6 -6" /></svg>
             </Link>
           </div>
